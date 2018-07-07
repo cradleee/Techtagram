be003dd98dbdfe8886efb9139e1a7e82db5d98f0
釜野由里佳 <yrkmn19@gmail.com>
2018-07-07 16:03:40 +0900

    ボタン

--- Techtagram/ViewController.swift
+++ Techtagram/ViewController.swift
@@ -16,11 +16,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //撮影する時のメソッド
+    @IBAction func useCamera(){
+        
+    }
+    
+    //表示している画像にフィルター加工をする時のメソッド
+    @IBAction func save() {
+        
+    }
 
+    //カメラロールにあるがそうを読み込むときのメソッド
+    @IBAction func openAlbum() {
+        
+    }
+    
+    //編集した画像をシェアする時のメソッド
+    @IBAction func share() {
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
